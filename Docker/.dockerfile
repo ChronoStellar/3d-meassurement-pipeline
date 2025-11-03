37012cee5245344fd062f1dadb2d8e378fd47eb3
ChronoStellar <hendrikcarlo0@gmail.com>
2025-11-03 08:16:34 +0700

    First Commit

---
+++ Docker/.dockerfile
@@ -0,0 +1,7 @@
+
+FROM python:3.12.12
+
+WORKDIR /3D-meassurement-pipeline/Docker
+
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
---
+++ scripts/prepare_data.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+mkdir -p data
+cd data
+gdown "https://drive.google.com/uc?id=1untXhYOLQtpNEy4GTY_0fL_H-k6cTf_r"
+unzip vibe_data.zip
+rm vibe_data.zip
+cd ..
+mv data/vibe_data/sample_video.mp4 .
+mkdir -p $HOME/.torch/models/
+mv data/vibe_data/yolov3.weights $HOME/.torch/models/
+
+gdown "https://drive.google.com/uc?id=1PLUUsB2w7EOc4Zr6QviQuLOL6NVFN_BI" -O data/smpl/SMPL_NEUTRAL.pkl
+gdown "https://drive.google.com/uc?id=1tVgCkqRVNsW8BtLKYeuqNUfT2PSxE6GL" -O data/smplx/SMPLX_NEUTRAL.pkl
